2125183c01bc1c164e1401f08c87a4a44a1613e5
Fahmii <202451222@std.umk.ac.id>
2026-01-01 07:55:24 +0700

    Delete footer.php

--- footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="text-center py-3">
-    © <?php echo date('Y'); ?> SUNDAY STREET COFFEE
-</footer>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
