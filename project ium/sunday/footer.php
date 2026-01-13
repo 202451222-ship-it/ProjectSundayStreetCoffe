7a0e0aa9a8df1fe245d39090fe53a1ad2ad117df
Fahmii <202451222@std.umk.ac.id>
2026-01-13 12:31:42 +0700

    Add files via upload

---
+++ project ium/sunday/footer.php
@@ -0,0 +1,7 @@
+<footer class="text-center py-3">
+    © <?php echo date('Y'); ?> SUNDAY STREET COFFEE
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
